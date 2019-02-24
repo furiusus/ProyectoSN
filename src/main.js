b88b840292cce6e2fba6f0d160e944207337ef78
furiusus <furiusus@localhost.localdomain>
2019-02-23 23:39:56 -0500

    muro 0.1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+if(localStorage.getItem("usuarios")==null){
+    var usuarios = [{"id":1,"nombres":"Ronny Daniel, Herrera Herrera","correo":"rherrerah@uni.pe","contrasenia":"ronny"},{"id":2,"nombres":"Mary Carmen, Herrera Herrera","correo":"mherrera@gmail.com","contrasenia":"mary"}]
+    localStorage.setItem("usuarios",JSON.stringify(usuarios));
+}
+if(localStorage.getItem("publicaciones")==null){
+    var publicaciones = [{"id_publicacion":1, "contenido":"Publicacion 1 con mucho contenido", "id_usuario":1,"listaUsuariosLike":[1]},{"id_publicacion":2, "contenido":"Publicacion 2 con mucho contenido", "id_usuario":1,"listaUsuariosLike":[1,2]},{"id_publicacion":3, "contenido":"Publicacion 3 con mucho contenido", "id_usuario":1,"listaUsuariosLike":[1,2]},{"id_publicacion":4, "contenido":"Publicacion 4 con mucho contenido", "id_usuario":1,"listaUsuariosLike":[2]},{"id_publicacion":5, "contenido":"Publicacion 5 con mucho contenido", "id_usuario":1,"listaUsuariosLike":[2]},{"id_publicacion":6, "contenido":"Publicacion 6 con mucho contenido", "id_usuario":2,"listaUsuariosLike":[2]},{"id_publicacion":7, "contenido":"Publicacion 7 con mucho contenido", "id_usuario":2,"listaUsuariosLike":[2]},{"id_publicacion":8, "contenido":"Publicacion 8 con mucho contenido", "id_usuario":2,"listaUsuariosLike":[1,2]},{"id_publicacion":9, "contenido":"Publicacion 9 con mucho contenido", "id_usuario":2,"listaUsuariosLike":[1,2]},{"id_publicacion":10, "contenido":"Publicacion 10 con mucho contenido", "id_usuario":2,"listaUsuariosLike":[1]}]
+    localStorage.setItem("publicaciones",JSON.stringify(publicaciones));
+}
 var r=ReactDOM;
 class Login extends React.Component{
     constructor(props){
@@ -8,7 +16,8 @@ class Login extends React.Component{
             entrar:false,
             mensajeCorreo:"",
             mensajePassword:"",
-            correo:""
+            correo:"",
+            id:0
         }
         this.correoChange=this.correoChange.bind(this);
         this.passChange = this.passChange.bind(this);
@@ -45,7 +54,7 @@ class Login extends React.Component{
     }
     validarPassword(password){
         if(password.length==0){
-            this.setState({mensajeCorreo:"Contraseña invalido"})
+            this.setState({mensajePassword:"Contraseña invalido"})
             return false;
         }else{
             return true;
@@ -58,7 +67,7 @@ class Login extends React.Component{
             for(var i=0 ; i< usuarios.length;i++){
                 if(usuarios[i].correo==this.state.correo){
                     if(usuarios[i].contrasenia==this.state.contrasenia){
-                        console.log("entro");
+                        this.setState({entrar:true,id:usuarios[i].id})
                         break;
                     }else{
                         this.setState({mensajePassword:"Contraseña incorrecta"})
@@ -78,7 +87,10 @@ class Login extends React.Component{
         const mensaje2 = !(this.state.mensajePassword=="")?(
             <div className="invalido">{this.state.mensajePassword}</div>
         ):<div className="valido"></div>;
-        const etiqueta = !this.state.entrar?(<div className="row container" >
+        const etiqueta = !this.state.entrar?(
+            <div>
+        <div ><br></br><br></br><br></br><br></br><br></br><br></br></div>
+        <div className="row container" >
         <div className="col-sm-2 col-lg-4"></div>
         <div className="col-12 col-sm-8 col-lg-4 fondoLogin">
             <br></br>
@@ -101,7 +113,9 @@ class Login extends React.Component{
             <br></br>
         </div>
         <div className="col-sm-2"></div>
-    </div>):<PaginaPrincipal first_name={this.state.nombre}></PaginaPrincipal>
+        </div>
+        <div><br></br><br></br><br></br><br></br><br></br><br></br></div>
+        </div>):<PaginaPrincipal id={this.state.id}></PaginaPrincipal>
         return(
             <div>
                 {etiqueta}
@@ -109,43 +123,126 @@ class Login extends React.Component{
         );
     }
 }
+function BarraPaginaPrincipal(props){
+    return(
+        <div className="opacity70 font-weight-bold">
+        <br></br>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">Natural Social Network</a>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                    <a className="nav-link" href="#" onClick={props.clickMostrarInfoPersonal}>Informacion Personal</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#" onClick={props.clickMostrarPublicacion}>Publicaciones</a>
+                </li>
+                </ul>
+                <form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        </div>
+    )
+}
+function InformacionPersonal(props){
+    return(
+        <div className="row">
+        <div className="col-lg-1"></div>
+        <div className="col-lg-10 col-md-12 col-sm-12">
+            <br></br><br></br>
+                <table className=" table table-dark opacity70 table table-hover ">
+                    <tbody>
+                        <tr>
+                            <td>Nombre:</td>
+                            <td>{props.usuario.nombres}</td>
+                        </tr>
+                        <tr>
+                            <td>Correo:</td>
+                            <td>{props.usuario.correo}</td>
+                        </tr>
+                    </tbody>
+                </table>
+        </div>
+        </div>
+    )
+}
+function Publicacion (props){
+    const etiq = props.lista.map(publicacion=>
+        <tr><td>{publicacion.contenido}</td></tr>
+    );
+    return(
+        <div className="row">
+        <div className="col-lg-1"></div>
+        <div className="col-lg-10 col-md-12 col-sm-12 ">
+        <br></br>
+        <table className="table table-dark opacity70 table-responsive">
+            <tbody>
+                <div className>
+                    {etiq}
+                </div>
+                
+            </tbody>
+        </table>
+        </div>
+        </div>
+    )
+}
 class PaginaPrincipal extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
         this.state={
-            persona:{
+            usuario:{
+                id:0,
+                nombres:"",
+                correo:""
+            },
+            verPerfil:false,
+            verPublicacion:false,
+            publicaciones:[]
+        };
+        this.mostrarInfoPersonal = this.mostrarInfoPersonal.bind(this);
+        this.mostrarPublicaciones = this.mostrarPublicaciones.bind(this);
+    }
+    componentDidMount(){
+        const publicaciones = JSON.parse(localStorage.getItem("publicaciones"));
+        this.setState({publicaciones})
+        var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+        for(var i=0;i<usuarios.length;i++){
+            if(usuarios[i].id==this.props.id){
+                var usuario = {
+                    id:usuarios[i].id,
+                    nombres:usuarios[i].nombres,
+                    correo:usuarios[i].correo
+                }
+                this.setState({usuario})
+                break;
             }
         }
     }
+    mostrarInfoPersonal(){
+        this.setState({verPerfil:true,verPublicacion:false})
+    }
+    mostrarPublicaciones(){
+        this.setState({verPerfil:false,verPublicacion:true})
+
+    }
     render(){
+        var lista =this.state.publicaciones;
+        var etiqInfo = this.state.verPerfil?(<InformacionPersonal usuario={this.state.usuario}></InformacionPersonal>):<div></div>
+        var etiqPubli = this.state.verPublicacion?(<Publicacion usuarioID={this.state.usuario.id} lista={lista}></Publicacion>):<div></div>
         return(
             <div>
-                <div>Estas adentro</div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a className="navbar-brand" href="#">Navbar</a>
-
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">{this.state.persona.first_name}<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                    </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-                </nav>
-            </div>
+                <BarraPaginaPrincipal clickMostrarInfoPersonal={this.mostrarInfoPersonal} 
+                clickMostrarPublicacion={this.mostrarPublicaciones}></BarraPaginaPrincipal>
+                <div>{etiqInfo}</div>
+                <div>{etiqPubli}</div>
+            </div>            
         )
     }
 }
@@ -153,11 +250,7 @@ class AplicacionWeb extends React.Component{
     constructor(props){
         super(props)
         this.state ={
-            
         }
-    }
-    visualizar(){
-
     }
     render(){
         return(
